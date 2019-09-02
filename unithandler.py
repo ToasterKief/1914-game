43f319daab9078d2f3423c9ba658ef2954c8d3cf
ToasterKief <48206697+ToasterKief@users.noreply.github.com>
2019-09-02 02:52:54 -0500

    Create unithandler.py

---
+++ unithandler.py
@@ -0,0 +1,17 @@
+class Soldier:
+	def __init__(self, name, description, rank, soldierrole, hp, status, stamina, lifestate, mood, turnsenlisted):
+		self.name = name
+		self.description = description
+		self.soldierrole = role
+		self.hp = 10
+		self.status = "idle"
+		self.stamina = 100
+		self.lifestate = "alive"
+		self.mood = "content"
+		self.turnsenlisted = 0
+		self.rank = "private"
+		
+		
+class Peasant(Soldier):
+		
+		
