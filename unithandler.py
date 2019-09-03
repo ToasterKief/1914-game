4ed22664a82753c107abe3757655714d2288878f
Toaster Zodin Vaen <toasterkief@localhost.localdomain>
2019-09-03 02:28:32 -0500

    Began creating classes for the infantry units in unithandler.py

--- unithandler.py
+++ unithandler.py
@@ -1,3 +1,7 @@
+from random import *
+from dice import *
+
+
 class Soldier:
 	def __init__(self, name, description, rank, soldierrole, hp, status, stamina, lifestate, mood, turnsenlisted):
 		self.name = name
@@ -80,9 +84,39 @@ class RangedCavalry(Soldier):
 class Peasant(MeleeInfantry):
 	def __init__(self, weapon, unitID, cellID, armor, weapondamage):
 		self.weapon = "pitchfork"
-		self.cellID = "infantry" + (cellNum + 1)
+		
 		self.unitID = "lightinfantry" + (peasantsNum + 1)
 		self.armor = "leather"
-		self.weapondamage = 
+		self.hitdie = dice.Dice(4)
+		super().__init__(name = "Peasant", description = "A low class citizen or farmer turned army fodder. They are weak and unorganized. They can be useful when unleashed in large formations and while not very effective on their own they can quickly overrun trained troops in mass.", rank, soldierrole = "Infantry(Melee)", hp, status, stamina, lifestate, mood, turnsenlisted, unitstyle = "Peasant Melee", Discipline, charge, attack, defense, mobility, awareness, morale, nerve, traits)
+		
+		 
+	def initStats(self):
+		discMod = random.randint(-3, 1)
+		chargeMod = random.randint(0, 3)
+		attackMod = random.randint(0, 2)
+		defenseMod = random.randint(0, 4)
+		mobilityMod = random.randint(-2, 3)
+		awarenessMod = random.randint(-5, 0)
+		moraleMod = random.randint(=4, 1)
+		
+		self.Discipline += discMod
+		self.charge += chargeMod
+		self.attack += attackMod
+		self.defense += defenseMod
+		self.mobility += mobilityMod
+		self.awareness += awarenessMod
+		self.morale += moraleMod
+		
+	def __str__(self):
+		return "{}\n=====\n{}\nRank: {}\nUnitID: {}\nStatus: {}\nMood: {}\n Turns In Service: {}\nWeapon: {}\nArmor: {}\nAttack: {}\nDefense: {}\nCharge: {}\nDiscipline: {}\nMobility: {}\nAwareness: {}\nMorale: {}\nNerve: {}\nTraits: {}\n".format(self.unitstyle, self.description, self.unitID, self.status, self.mood, self.turnsenlisted, self.weapon, self.armor, self.attack, self.defense, self.charge, self.Discipline, self.Mobility, self.awareness, self.morale, self.nerve, self.traits)
+		
+class SpearmanMilitia(MeleeInfantry):
+	def __init__(self, weapon, unitID, cellID, armor, weapondamage):
+		self.weapon = "spear"
+		self.unitID = "spearmanmilitia" + (spearNum + 1)
+		self.armor = "leather"
+		self.hitdie = dice.Dice(6)
+		super().__init__(name = "Spearman Militia", description = "A militarized civilian with minimal gear and wielding a spear. They aren't very effective in charges and other offensive strategies, but can form defensive lines 
 
-			       
+	
